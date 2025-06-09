43792ce18d2bf9d0d3421a4e2cad5c15d158de27
84In <trungtin7166293@gmail.com>
2025-06-09 20:22:33 +0700

    fix(bac-si): adjust DoctorDetail page to fix TypeScript type error on params for Next.js 15 build - 2nd time

--- src/app/doi-ngu-bac-si/[slug]/page.tsx
+++ src/app/doi-ngu-bac-si/[slug]/page.tsx
@@ -1,12 +1,10 @@
 import { DoctorDetailPage } from "@/components/doctors/doctor-detail-page";
 
-type DoctorDetailProps = {
-  params: {
-    slug: string;
-  };
-};
-
-export default async function DoctorDetail({ params }: DoctorDetailProps) {
+export default async function DoctorDetail({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   // Nếu bạn cần fetch data async ở đây thì để async
-  return <DoctorDetailPage slug={params.slug} />;
+  return <DoctorDetailPage slug={(await params).slug} />;
 }
